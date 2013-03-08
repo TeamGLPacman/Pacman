733ca5b82c0086c462525bd36d2460c6f82130bd
jorre92 <jorre92@hotmail.com>
2013-03-08 11:32:46 +0100

    HUNT:
    1/5 går ifrån pacman
    4/5 går mot Pacman
    SCARED:
    1/5 går mot pacman
    4/5 går från pacman

--- Pacman/Hunt.cpp
+++ Pacman/Hunt.cpp
@@ -22,6 +22,75 @@ Hunt::~Hunt()
 
 vec3 Hunt::Update(int sur[4], vec3 *worldPos, float speed, vec3 *target)
 {
+	
+	vec3 distancefrompacman = *mPacmanPos - *worldPos;
+	vector <int> vLeft, vRight, vUp, vDown;
+	float lengthx = distancefrompacman.x; //  x
+	float lengthz = distancefrompacman.z; // "y"
+	int i = 0;
+	if(lengthx < 0)
+	{
+		while(i < 6)
+		{
+			vLeft.push_back(i);
+			i++;
+		}
+		vRight.push_back(i);
+		i++;
+	}
+	else if (lengthx > 0)
+	{
+		while(i < 6)
+		{
+			vRight.push_back(i);
+			i++;
+		}
+		vLeft.push_back(i);
+		i++;
+	}
+	else
+	{
+		while(i < 7)
+		{
+			vRight.push_back(i);
+			i++;
+			vLeft.push_back(i);
+			i++;
+		}
+		
+	}
+	if(lengthz < 0)
+	{
+		while(i < 13)
+		{ 
+			vUp.push_back(i);
+			i++;
+		}
+		vDown.push_back(i);
+		i++;
+	}
+	else if (lengthx > 0)
+	{
+		while(i < 13)
+		{
+			vDown.push_back(i);
+			i++;
+		}
+		vUp.push_back(i);
+		i++;
+	}
+	else
+	{
+		while(i < 14)
+		{
+			vDown.push_back(i);
+			i++;
+			vUp.push_back(i);
+			i++;
+		}
+	}
+	
+	
 	if (*worldPos == *target)
 	{ 
 		if ((mLastDir.x == 1 && sur[0] == 1 && sur[2] == 1 && sur[1] != 1)
@@ -34,34 +103,61 @@ vec3 Hunt::Update(int sur[4], vec3 *worldPos, float speed, vec3 *target)
 		}
 		else
 		{
-		int i;
+			int i;
 			while (true)
 			{
-				i = rand()%4;
-				if ( i == 0 && sur[0] != 1)
+				i = rand()%14;
+				if (sur[0] != 1)
 				{
-					mLastDir = vec3(0,0,-1);
-					break;
+					for (int j = 0; j < vUp.size(); j++)
+					{
+						if(i == vUp[j])
+						{
+							mLastDir = vec3(0,0,-1);
+							*target += mLastDir;
+							return mLastDir;
+						}
+					}
 				}
-				else if ( i == 1 && sur[1] != 1)
+				if (sur[2] != 1)
 				{
-					mLastDir = vec3(1,0,0);
-					break;
+					for (int j = 0; j < vDown.size(); j++)
+					{
+						if(i == vDown[j])
+						{
+							mLastDir = vec3(0,0,1);
+							*target += mLastDir;
+							return mLastDir;
+						}
+					}
 				}
-				else if ( i == 2 && sur[2] != 1)
+				if (sur[3] != 1)
 				{
-					mLastDir = vec3(0,0,1);
-					break;
+					for (int j = 0; j < vLeft.size(); j++)
+					{
+						if(i == vLeft[j])
+						{
+							mLastDir = vec3(-1,0,0);
+							*target += mLastDir;
+							return mLastDir;
+						}
+					}
 				}
-				else if ( i == 3 && sur[3] != 1)
+				if (sur[1] != 1)
 				{
-					mLastDir = vec3(-1,0,0);
-					break;
+					for (int j = 0; j < vRight.size(); j++)
+					{
+						if(i == vRight[j])
+						{
+							mLastDir = vec3(1,0,0);
+							*target += mLastDir;
+							return mLastDir;
+						}
+					}
 				}
 			}
 		}
-		*target += mLastDir;
-		return mLastDir;
+
 	}
 	else
 	{
